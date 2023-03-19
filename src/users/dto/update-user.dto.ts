b1c95580c14acd706bc43544b93ef8f2518e423b
bam <keskinemirhan@gmail.com>
2023-03-19 18:35:12 +0300

    Add User dtos

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user.dto";
+import { IsString } from "class-validator";
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  email?: string;
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+  @IsString()
+  username: string;
+
+  @IsString()
+  password?: string;
+}
