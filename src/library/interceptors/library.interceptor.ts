51abec31e88636e511fac709f8346c362ff072dd
bam <keskinemirhan@gmail.com>
2023-03-20 14:00:00 +0300

    Add library interceptor to attach library of logged in user

---
+++ src/library/interceptors/library.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+import { LibraryService } from "../library.service";
+
+//Interceptor to assign library entity to request object
+//It takes user object from request
+//JwtAuthGuard assigns logged in user to request object
+@Injectable()
+export class LibraryInterceptor implements NestInterceptor {
+  constructor(private libraryService: LibraryService) {}
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    const req = context.switchToHttp().getRequest();
+    req.library = await this.libraryService.findByUser(req.user);
+    return next.handle();
+  }
+}
