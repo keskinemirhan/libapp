1fb02ca88b1b89d14261893509246b5f74eac453
bam <keskinemirhan@gmail.com>
2023-03-20 19:04:05 +0300

    Add Interceptor to Library Module

--- src/library/library.module.ts
+++ src/library/library.module.ts
@@ -6,13 +6,21 @@ import { Library } from "./entities/library.entity";
 import { Book } from "./entities/book.entity";
 import { Category } from "./entities/category.entity";
 import { UsersModule } from "src/users/users.module";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { LibraryInterceptor } from "./interceptors/library.interceptor";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Library, Book, Category]),
     forwardRef(() => UsersModule),
   ],
-  providers: [LibraryService],
+  providers: [
+    LibraryService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LibraryInterceptor,
+    },
+  ],
   controllers: [LibraryController],
   exports: [LibraryService],
 })
