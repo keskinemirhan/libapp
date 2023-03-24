062bb44891d08d8ce1144f2275152131976b4a58
bam <keskinemirhan@gmail.com>
2023-03-24 14:42:17 +0300

    Use cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+let cors = require("cors");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
