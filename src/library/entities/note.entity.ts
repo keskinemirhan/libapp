fcce8c4e92e8435dec5118ce3c08591b4fd9dd95
bam <keskinemirhan@gmail.com>
2023-03-28 21:46:51 +0300

    Add book entity

---
+++ src/library/entities/note.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  UpdateDateColumn,
+} from "typeorm";
+import { Book } from "./book.entity";
+
+@Entity()
+export class Note {
+  @Column()
+  title: string;
+
+  @Column()
+  note: string;
+
+  @ManyToOne(() => Book, (book) => book.notes)
+  book: Book[];
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
