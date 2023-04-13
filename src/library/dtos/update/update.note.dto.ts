3426bed832843455b823ec2f98ed4e8ba106d0f3
cppmonger <keskinemirhan@gmail.com>
2023-04-13 04:05:53 +0300

    (chore)

--- src/library/dtos/update/update.note.dto.ts
+++ src/library/dtos/update/update.note.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString, MaxLength } from "class-validator";
+import { IsNumber, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UpdateNoteDto {
   @IsNumber()
@@ -6,6 +6,7 @@ export class UpdateNoteDto {
 
   @IsString()
   @MaxLength(256)
+  @MinLength(1)
   title: string;
 
   @IsString()
